83a04fe066cf0e8369b58917e29fec3ba588d02f
Dimas <ahmaddimas5151@gmail.com>
2025-08-23 15:21:47 +0700

    new: table view

--- application/views/admin/guru/index.php
+++ application/views/admin/guru/index.php
@@ -1,33 +1,112 @@
-<div class="card shadow mb-4">
-    <div class="card-header d-flex justify-content-between align-items-center">
-        <h6 class="m-0 font-weight-bold text-primary">Setup Akun Guru</h6>
-        <a href="<?= site_url('admin/guru/create'); ?>" class="btn btn-sm btn-primary">Tambah Guru</a>
-    </div>
-    <div class="card-body">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>NIP/ID</th>
-                    <th>Nama</th>
-                    <th>Jenis Kelamin</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($user_list as $user): ?>
-                    <tr>
-                        <td><?= $user->user_id; ?></td>
-                        <td><?= htmlspecialchars($user->nama); ?></td>
-                        <td><?= ($user->jk == 'L') ? 'Laki-laki' : 'Perempuan'; ?></td>
-                        <td>
-                            <a href="<?= site_url('admin/guru/edit/' . $user->user_id); ?>"
-                                class="btn btn-sm btn-info">Edit</a>
-                            <a href="<?= site_url('admin/guru/delete/' . $user->user_id); ?>" class="btn btn-sm btn-danger"
-                                onclick="return confirm('Yakin?');">Hapus</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+    <div class="d-flex flex-column flex-column-fluid">
+        <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-0">
+            <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
+                <div class="page-title d-flex flex-column justify-content-center me-3">
+                    <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
+                        Manajemen Guru
+                    </h1>
+                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                        <li class="breadcrumb-item text-muted">
+                            <a href="<?= site_url('admin/dashboard'); ?>" class="text-muted text-hover-primary">Beranda</a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+                        </li>
+                        <li class="breadcrumb-item text-muted">Data Guru</li>
+                    </ul>
+                </div>
+                <div class="d-flex align-items-center gap-2 gap-lg-3">
+                    <a href="<?= site_url('admin/guru/create'); ?>" class="btn btn-sm fw-bold btn-primary">
+                        <i class="ki-outline ki-plus fs-2"></i>Tambah Guru
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <div id="kt_app_content_container" class="app-container container-xxl">
+                <div class="card card-flush">
+                    <div class="card-header align-items-center py-5 gap-2 gap-md-5">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center position-relative my-1">
+                                <i class="ki-outline ki-magnifier fs-3 position-absolute ms-4"></i>
+                                <input type="text" data-kt-ecommerce-order-filter="search"
+                                    class="form-control form-control-solid w-250px ps-12" placeholder="Cari Guru" />
+                            </div>
+                        </div>
+
+                        <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
+                            <button type="button" class="btn btn-light-primary" data-kt-menu-trigger="click"
+                                data-kt-menu-placement="bottom-end">
+                                <i class="ki-outline ki-exit-up fs-2"></i>Export Report</button>
+                            <div id="kt_ecommerce_report_customer_orders_export_menu"
+                                class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-200px py-4"
+                                data-kt-menu="true">
+                                <div class="menu-item px-3">
+                                    <a href="#" class="menu-link px-3" data-kt-ecommerce-export="copy">Copy to
+                                        clipboard</a>
+                                </div>
+                                <div class="menu-item px-3">
+                                    <a href="#" class="menu-link px-3" data-kt-ecommerce-export="excel">Export as
+                                        Excel</a>
+                                </div>
+                                <div class="menu-item px-3">
+                                    <a href="#" class="menu-link px-3" data-kt-ecommerce-export="csv">Export as CSV</a>
+                                </div>
+                                <div class="menu-item px-3">
+                                    <a href="#" class="menu-link px-3" data-kt-ecommerce-export="pdf">Export as PDF</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body pt-0">
+                        <table class="table align-middle table-row-dashed fs-6 gy-5"
+                            id="kt_ecommerce_report_customer_orders_table">
+                            <thead>
+                                <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                                    <th class="min-w-100px">ID</th>
+                                    <th class="min-w-100px">Nama</th>
+                                    <th class="min-w-100px">Jenis Kelamin</th>
+                                    <th class="text-end min-w-100px">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody class="fw-semibold text-gray-600">
+                                <?php foreach ($user_list as $user): ?>
+                                    <tr>
+                                        <td><?= $user->user_id; ?></td>
+                                        <td><?= htmlspecialchars($user->nama); ?></td>
+                                        <td><?= ($user->jk == 'L') ? 'Laki-laki' : 'Perempuan'; ?></td>
+                                        <td class="text-end">
+                                            <a href="#"
+                                                class="btn btn-sm btn-light btn-flex btn-center btn-active-light-primary"
+                                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                                Aksi
+                                                <i class="ki-outline ki-down fs-5 ms-1"></i> </a>
+                                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
+                                                data-kt-menu="true">
+                                                <div class="menu-item px-3">
+                                                    <a href="<?= site_url('admin/guru/edit/' . $user->user_id); ?>"
+                                                        class="menu-link px-3">
+                                                        Ubah
+                                                    </a>
+                                                </div>
+
+                                                <div class="menu-item px-3">
+                                                    <a href="<?= site_url('admin/guru/delete/' . $user->user_id); ?>"
+                                                        class="menu-link px-3"
+                                                        data-kt-ecommerce-product-filter="delete_row">
+                                                        Hapus
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- application/views/templates/nav_admin.php
+++ application/views/templates/nav_admin.php
@@ -2,7 +2,7 @@
     data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="275px"
     data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_toggle">
     <div class="d-flex flex-stack px-4 px-lg-6 py-3 py-lg-8" id="kt_app_sidebar_logo">
-        <a href="../../demo23/dist/index.html">
+        <a href="<?= site_url('admin/dashboard'); ?>">
             <img alt="Logo" src="assets/media/logos/demo23.svg" class="h-20px h-lg-25px theme-light-show" />
             <img alt="Logo" src="assets/media/logos/demo23-dark.svg" class="h-20px h-lg-25px theme-dark-show" />
         </a>
@@ -17,37 +17,92 @@
                 <h3 class="text-gray-800 fw-bold mb-8">Services</h3>
                 <div class="row row-cols-3" data-kt-buttons="true" data-kt-buttons-target="[data-kt-button]">
                     <div class="col mb-4">
-                        <a href="../../demo23/dist/apps/calendar.html"
-                            class="btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                        <a href="<?= site_url('admin/dashboard'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'dashboard') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
                             data-kt-button="true">
                             <span class="mb-2">
-                                <i class="ki-outline ki-calendar fs-1"></i>
+                                <i class="ki-outline ki-home-2 fs-1"></i>
                             </span>
-                            <span class="fs-7 fw-bold">Events</span>
+                            <span class="fs-7 fw-bold">Dasbor</span>
                         </a>
                     </div>
                     <div class="col mb-4">
-                        <a href="../../demo23/dist/apps/support-center/licenses.html"
-                            class="btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                        <a href="<?= site_url('admin/kategori'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'kategori') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
                             data-kt-button="true">
                             <span class="mb-2">
-                                <i class="ki-outline ki-security-check fs-1"></i>
+                                <i class="ki-outline ki-star fs-1"></i>
                             </span>
-                            <span class="fs-7 fw-bold">Insurance</span>
+                            <span class="fs-7 fw-bold">Kategori</span>
                         </a>
                     </div>
                     <div class="col mb-4">
-                        <a href="../../demo23/dist/apps/contacts/add-contact.html"
-                            class="btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px active border-primary border-dashed"
+                        <a href="<?= site_url('admin/status'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'status') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
                             data-kt-button="true">
                             <span class="mb-2">
-                                <i class="ki-outline ki-plus fs-1"></i>
+                                <i class="ki-outline ki-star fs-1"></i>
                             </span>
-                            <span class="fs-7 fw-bold">Add New</span>
+                            <span class="fs-7 fw-bold">Status</span>
+                        </a>
+                    </div>
+                    <div class="col mb-4">
+                        <a href="<?= site_url('admin/siswa'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'siswa') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                            data-kt-button="true">
+                            <span class="mb-2">
+                                <i class="ki-outline ki-book fs-1"></i>
+                            </span>
+                            <span class="fs-7 fw-bold">Siswa</span>
+                        </a>
+                    </div>
+                    <div class="col mb-4">
+                        <a href="<?= site_url('admin/guru'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'guru') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                            data-kt-button="true">
+                            <span class="mb-2">
+                                <i class="ki-outline ki-teacher fs-1"></i>
+                            </span>
+                            <span class="fs-7 fw-bold">Guru</span>
+                        </a>
+                    </div>
+                    <div class="col mb-4">
+                        <a href="<?= site_url('admin/manajemen'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'manajemen') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                            data-kt-button="true">
+                            <span class="mb-2">
+                                <i class="ki-outline ki-award fs-1"></i>
+                            </span>
+                            <span class="fs-7 fw-bold">Manajemen</span>
+                        </a>
+                    </div>
+                </div>
+
+                <h3 class="text-gray-800 fw-bold mt-8 mb-8">Laporan</h3>
+                <div class="row row-cols-3" data-kt-buttons="true" data-kt-buttons-target="[data-kt-button]">
+                    <div class="col mb-4">
+                        <a href="<?= site_url('admin/manajemen'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'manajemen') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                            data-kt-button="true">
+                            <span class="mb-2">
+                                <i class="ki-outline ki-award fs-1"></i>
+                            </span>
+                            <span class="fs-7 fw-bold">Pengaduan</span>
+                        </a>
+                    </div>
+                    <div class="col mb-4">
+                        <a href="<?= site_url('admin/manajemen'); ?>"
+                            class="<?= ($this->uri->segment(2) == 'manajemen') ? 'active' : ''; ?> btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-lg-90px h-lg-90px w-70px h-70px border-gray-200"
+                            data-kt-button="true">
+                            <span class="mb-2">
+                                <i class="ki-outline ki-award fs-1"></i>
+                            </span>
+                            <span class="fs-7 fw-bold">Monitoring</span>
                         </a>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
